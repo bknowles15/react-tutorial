a1d8dbe832b971fdbb8d26384a101824889fa114
Bronson Knowles <bknowlesgit@gmail.com>
2021-02-24 17:46:19 -0500

    Remove commented code

--- src/Form.js
+++ src/Form.js
@@ -31,35 +31,4 @@ const Form = (props) => {
     );
 }
 
-
-// const Form = (props) => {
-//     const [title, setTitle] = useState('');
-
-//     const handleChange = (event) => {
-//         setTitle(event.target.value);
-//     }
-
-//     const onFormSubmit = (event) => {
-//         event.preventDefault();
-        
-//         props.handleSubmit(title);
-//         setTitle('');
-//     }
-
-//     return (
-//         <form onSubmit={onFormSubmit}>
-//             <label>Title</label>
-//             <input 
-//                 type="text" 
-//                 title="title" 
-//                 id="name"
-//                 value={title} 
-//                 onChange={handleChange} />
-//             <button type="submit">
-//                 Submit
-//             </button>
-//         </form>
-//     );
-// }
-
 export default Form;
